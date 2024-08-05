9c74bb89c9563d81986fc3b96e69dd8bb74f908e
Prasad Vaidya <vaidyaprasad84@gmail.com>
2024-08-04 21:28:55 -0500

    Added linear MTA and comments

--- vipanan/mta/heuristics/heuristics.py
+++ vipanan/mta/heuristics/heuristics.py
@@ -66,35 +66,49 @@ class heuristics:
                                    dataframe = pd.DataFrame({}),
                                    decay_type='exponential',
                                    A = 1):
+        '''
+        Here a default value of A = 1 is added. The user has an option to of course change the A.
+        But changing A can lead to different solution and A = 1 is recommended. 
+        Please refer to documentation for more details. 
+        
+        There are 3 options for decay time. 
+        1. Exponential --> The last channel has maximum attribution and the attribution decays exponentially as go from last to first
+        2. Linear --> The last channel has maximum attribution and the attribution decays linearly as go from last to first
+        3. None --> There is no time decay. Meaning all channels have equal attribution. This is same as linear MTA model.
+        '''
         
         conv_df = dataframe[dataframe['conversion']==1][['user_id']]
         conv_df = pd.merge(dataframe,conv_df,on = 'user_id', how = 'inner')
         conv_df1 = conv_df.groupby(['user_id']).size().reset_index().rename(columns = {0:'count'})
+        conv_attr = []
         
-        if decay_type == 'linear':
-            delta
-        elif decay_type == 'exponential':
-            d = dict({1:[1]})
-            n = 1
+        if decay_type == 'exponential': # Exponenial decay model
+            d = dict({1:[1]}) # When there is just one channel, it will get 100% attribution
+            n = 1 # This is just a dummy value added to ensure the next function runs. Its like a default value of n. 
             def func(x):
                 return (1-np.exp((n+1)*x[0]))/(1-np.exp(x[0])) - (1+A)/A
             
-            exp_conv_attr = []
             for i in range(len(conv_df1)):
                 n = conv_df1.iloc[i]['count']
-                if n not in d:
+                if n not in d: # the look up is applied to minize computation
                     k = fsolve(func,[1])[0]
                     wt = []
                     for j in range(1,n+1):
                         wt.append(round(A*np.exp(k*j),3))
-                    wt.reverse()
+                    wt.reverse() # A reverse function has to be applied. Because the fn gives value from last channel to first while input data is sored from first to last.
                     d[n] = wt
-                exp_conv_attr += d[n]
+                conv_attr += d[n]
+                
+        elif decay_type == 'linear': # Linear decay model
+            ans = 1
+        elif decay_type == None: #Linear MTA
+            for i in range(len(conv_df1)):
+                n = conv_df1.iloc[i]['count']
+                conv_attr += [round(1/n,3)]*n
             
-            conv_df['exp_conv_attr'] = exp_conv_attr
-            
-            ans = pd.DataFrame(conv_df.groupby(['channel'])['exp_conv_attr'].sum()).reset_index()
-            
-            return ans
+        conv_df['conv_attr'] = conv_attr
+        ans = pd.DataFrame(conv_df.groupby(['channel'])['conv_attr'].sum()).reset_index()   
+        return ans
                                  
 heuristics = heuristics()
+
